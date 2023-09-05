124ac926b6f3d4e66c9fc83b073234e4a5ac9b8d
Charlie Greenman <charlie@razroo.com>
2023-09-05 01:42:36 -0400

    Add code for angular-16.2.0 create-workspace

---
+++ apps/devgen-test-fiftyeuqi/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'devgen-test-fiftyeuqi-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'devgen-test-fiftyeuqi';
+}
